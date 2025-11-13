134d6cd2cc0c025431d2fc8c64f121b6b5b02126
visionthex <visionthex@pop-os.mynetworksettings.com>
2025-11-13 18:21:44 -0500

    first commit

---
+++ infinite_loop.s
@@ -0,0 +1,7 @@
+.global _start # Infinite loop program
+.section .text
+
+_start:
+    mov r0, pc
+    sub r0, r0, #8
+    bx r0
---
+++ test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+	printf("hey there this is ARM:)\n");
+	return 0;
+}
